8f55f08bd2d1d96a70e8b5e5663afc3a562f7919
Emanuel Rios Roque <emanuel.roque.web@gmail.com>
2024-05-02 12:20:03 -0600

    mysql

--- config/database.php
+++ config/database.php
@@ -45,22 +45,25 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '172.19.223.119'),
+            'port' => env('DB_PORT', '6033'),
+            'database' => env('DB_DATABASE', 'AnvizDB'),
+            'username' => env('DB_USERNAME', 'usr_anviz'),
+            'password' => env('DB_PASSWORD', '.fx4R$hP7Z6*'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'modes'  => [
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ],
         ],
 
         'pgsql' => [
