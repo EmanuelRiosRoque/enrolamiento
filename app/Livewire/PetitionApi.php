2b8fc1e8c803cbe895cc4cc7e11b80bd9b31fa55
Emanuel Rios Roque <emanuel.roque.web@gmail.com>
2024-05-03 08:39:51 -0600

    documento

--- app/Livewire/PetitionApi.php
+++ app/Livewire/PetitionApi.php
@@ -15,41 +15,41 @@ class PetitionApi extends Component
 
     public function fetchData()
     {
-        dd(phpinfo());
-        phpinfo();
-
-        // // Verifica si el número de empleado ya existe en la tabla update_empleado
-        // $empleadoExistente = UpdateEmpleados::where('nuM_EMPL', $this->nEmpleado)->exists();
-
-        // if ($empleadoExistente) {
-        //     $this->error = 'Este empleado ya fue dado de alta.';
-        //     return;
-        // }
-
-        // // Continúa con la lógica de la solicitud API solo si el empleado no existe en la base de datos local
-        // $this->showLoader();
-
-        // // Simula una solicitud de datos con un retraso de 3 segundos
-        // sleep(3);
-
-        // if (!$this->nEmpleado) {
-        //     return;
-        // }
-
-        // $response = Http::get('http://172.19.202.43/WebServices/META/api/Empleado?NumEmpleado=', [
-        //     'NumEmpleado' => $this->nEmpleado
-        // ]);
+        // dd(phpinfo());
 
 
-        // if ($response->ok()) {
-        //     $this->responseData = $response->json();
-        //     $this->hideLoader();
-        //     $this->error = null; // Resetea el mensaje de error en caso de éxito
-        // } else {
-        //     $this->responseData = [];
-        //     $this->hideLoader();
-        //     $this->error = 'Error al obtener datos. Por favor, inténtelo de nuevo más tarde.'; // Establece el mensaje de error
-        // }
+        // Verifica si el número de empleado ya existe en la tabla update_empleado
+        $empleadoExistente = UpdateEmpleados::where('nuM_EMPL', $this->nEmpleado)->exists();
+
+        if ($empleadoExistente) {
+            $this->error = 'Este empleado ya fue dado de alta.';
+            return;
+        }
+
+        // Continúa con la lógica de la solicitud API solo si el empleado no existe en la base de datos local
+        $this->showLoader();
+
+        // Simula una solicitud de datos con un retraso de 3 segundos
+        sleep(3);
+
+        if (!$this->nEmpleado) {
+            return;
+        }
+
+        $response = Http::get('http://172.19.202.43/WebServices/META/api/Empleado?NumEmpleado=', [
+            'NumEmpleado' => $this->nEmpleado
+        ]);
+
+
+        if ($response->ok()) {
+            $this->responseData = $response->json();
+            $this->hideLoader();
+            $this->error = null; // Resetea el mensaje de error en caso de éxito
+        } else {
+            $this->responseData = [];
+            $this->hideLoader();
+            $this->error = 'Error al obtener datos. Por favor, inténtelo de nuevo más tarde.'; // Establece el mensaje de error
+        }
     }
 
 
