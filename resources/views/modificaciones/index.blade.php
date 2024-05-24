f49d5f07d7fa3b5134e61782f49ef7cb08a63dd6
Emanuel Rios Roque <emanuel.roque.web@gmail.com>
2024-05-24 13:52:04 -0600

    Add Modificaciones

--- resources/views/modificaciones/index.blade.php
+++ resources/views/modificaciones/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Modificación del Empleado') }}
+            {{ __('Modificación del Empleado enrolado') }}
         </h2>
     </x-slot>
 
